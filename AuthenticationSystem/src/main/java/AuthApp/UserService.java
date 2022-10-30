b16fc2626b594864fb6de9c813aaf7200e45dc3a
nitza <nitzan.la4@gmail.com>
2022-10-30 15:35:05 +0200

    implemention of Client, User,UserService classes

--- AuthenticationSystem/src/main/java/AuthApp/UserService.java
+++ AuthenticationSystem/src/main/java/AuthApp/UserService.java
@@ -1,4 +1,34 @@
 package AuthApp;
 
+import java.util.Optional;
+
 public class UserService {
+
+    public boolean mailConfirmed (String mail)
+    {
+        if (getUserByEmail(mail).equal((Optional.empty())))
+            return true;
+        else return false;
+    }
+
+    public boolean idConfirmed (int id)
+    {
+        if (getUser(id).equal((Optional.empty())))
+            return true;
+        else return false;
+    }
+
+
+
+    public static String generateToken(int stringLength) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder sb = new StringBuilder(stringLength);
+
+        for(int i = 0; i < stringLength; ++i) {
+            int index = (int)((double)AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
 }
