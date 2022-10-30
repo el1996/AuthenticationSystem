fdba04f341bf5895f902436b9275b357034f3598
nitza <nitzan.la4@gmail.com>
2022-10-30 17:07:14 +0200

    UserService and AuthApp/AuthenticationService implementation

--- AuthenticationSystem/src/main/java/AuthApp/AuthenticationService.java
+++ AuthenticationSystem/src/main/java/AuthApp/AuthenticationService.java
@@ -1,7 +1,49 @@
 package AuthApp;
 
+import java.util.Optional;
+
 public class AuthenticationService {
 
+    private static UserRepository singleInstance = null;
+
+    private AuthenticationService() {UserRepository = AuthApp.UserRepository.getInstance();}
+
+    public static UserRepository getInstance() {
+        if (singleInstance == null) {
+            singleInstance = new UserRepository();
+        }
+
+        return singleInstance;
+    }
+
+
+    public boolean isEmailConfirmed (String mail)
+    {
+        if (singleInstance.getUserByEmail(mail).equal((Optional.empty())))
+            return true;
+        else return false;
+    }
+
+    public boolean isIdConfirmed (int id)
+    {
+        if (singleInstance.getUser(id).equal((Optional.empty())))
+            return true;
+        else return false;
+    }
+
+
+
+    public static String generateToken(int stringLength) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder sb = new StringBuilder(stringLength);
+
+        for(int i = 0; i < stringLength; ++i) {
+            int index = (int)((double)AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
 
 
 
