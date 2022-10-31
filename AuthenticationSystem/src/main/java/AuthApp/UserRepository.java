5de64e5d2facac0204ff6fcf4acae5e3a2ce7e6a
Lior Mathan <Lior.mathan@gmail.com>
2022-10-31 15:49:36 +0200

    Changed: saving users to seperate json files.

--- AuthenticationSystem/src/main/java/AuthApp/UserRepository.java
+++ AuthenticationSystem/src/main/java/AuthApp/UserRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 public class UserRepository {
     private static UserRepository singleInstance = null;
-    private final String usersFilepath = "users.json";
+    private final String usersFilepath = "UsersDB";
     private Map<Integer, User> usersMap;
 
 
@@ -38,22 +38,12 @@ public class UserRepository {
 
     public void addUser(User user) {
         usersMap.put(user.getId(), user);
-        saveToFile();
+        writeToFile(user);
     }
 
-    public void updatedUserName(int id, String name) {
-        this.usersMap.get(id).setName(name);
-        saveToFile();
-    }
-
-    public void updateUserPassword(int id, String password) {
-        this.usersMap.get(id).setPassword(password);
-        saveToFile();
-    }
-
-    public void updateUserEmail(int id, String email) {
-        this.usersMap.get(id).setEmail(email);
-        saveToFile();
+    public void updatedUser(User user) {
+        this.usersMap.put(user.getId(), user);
+        writeToFile(user);
     }
 
     public Optional<User> getUserByEmail(String email) {
@@ -66,33 +56,41 @@ public class UserRepository {
         return Optional.empty();
     }
 
-    public void deleteUser(int id) {
-        this.usersMap.remove(id);
-        saveToFile();
+    public void deleteUser(User user) {
+        this.usersMap.remove(user.getId());
+        File userFile = new File(getUserFilepath(user));
+        userFile.delete();
+    }
+
+    private String getUserFilepath(User user) {
+        return this.usersFilepath + File.separator + user.getId() + ".json";
     }
 
     public void parseConfigToMap() {
         this.usersMap = new HashMap<>();
 
-        try (FileInputStream fileInputStream = new FileInputStream(this.usersFilepath)) {
-            Gson gson = new Gson();
-            JsonReader jsonReader = new JsonReader(new InputStreamReader(fileInputStream));
-            Type mapType = new TypeToken<Map<Integer, User>>(){}.getType();
-            this.usersMap = gson.fromJson(jsonReader, mapType);
+        final File folder = new File(this.usersFilepath);
+        for (final File fileEntry : folder.listFiles()) {
+            try (FileInputStream fileInputStream = new FileInputStream(fileEntry.getPath())) {
+                Gson gson = new Gson();
+                JsonReader jsonReader = new JsonReader(new InputStreamReader(fileInputStream));
+                User user = gson.fromJson(jsonReader, User.class);
+                this.usersMap.put(user.getId(), user);
+            } catch (IOException ex) {
+                throw new RuntimeException(String.format("Error occurred while trying to open: %s", fileEntry.getPath()));
+            }
 
-        } catch (FileNotFoundException ex) {
-            saveToFile();
-        } catch (IOException ex) {
-            throw new RuntimeException(String.format("Error occurred while trying to open: %s", this.usersFilepath));
         }
     }
 
-    private void saveToFile() {
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.usersFilepath)))) {
+    private void writeToFile(User user) {
+        String absoluteFilePath = getUserFilepath(user);
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(absoluteFilePath)))) {
             Gson gson = new Gson();
-            gson.toJson(this.usersMap, writer);
+            gson.toJson(user, writer);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Filename: \"" + this.usersFilepath + "\" was not found.");
+            throw new RuntimeException("Filename: \"" + absoluteFilePath + "\" was not found.");
         } catch (IOException e) {
             throw new RuntimeException("Error occurred while trying to open new default json file.");
         }
